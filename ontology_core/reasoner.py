d9784a4378bd141ed27e50cbe44ba5d2fccbba08
Orest Yackulyak <126258463+OrestRocky@users.noreply.github.com>
2025-03-27 20:40:24 +0400

    Refactor: Rename Concept to OntologyConcept and add multiple inheritance

--- ontology_core/reasoner.py
+++ ontology_core/reasoner.py
@@ -1,18 +1,25 @@
 # reasoner.py
-# Simple symbolic reasoner for concept hierarchy
+# Refactored symbolic reasoner with multiple inheritance support
 
-class Concept:
-    def __init__(self, name, parent=None):
+class OntologyConcept:
+    def __init__(self, name, parents=None):
         self.name = name
-        self.parent = parent
+        self.parents = parents if parents else []
 
     def is_a(self, concept_name):
         if self.name == concept_name:
             return True
-        elif self.parent:
-            return self.parent.is_a(concept_name)
+        for parent in self.parents:
+            if parent.is_a(concept_name):
+                return True
         return False
 
-# Example
-human = Concept("Human", Concept("Mammal", Concept("Animal")))
-print(human.is_a("Animal"))  # True
+# Examples
+animal = OntologyConcept("Animal")
+mammal = OntologyConcept("Mammal", [animal])
+human = OntologyConcept("Human", [mammal])
+soul = OntologyConcept("Soul")
+ai_entity = OntologyConcept("AIEntity", [human, soul])
+
+print(ai_entity.is_a("Animal"))  # True
+print(ai_entity.is_a("Soul"))    # True
